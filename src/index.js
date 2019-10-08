db0d634824dc5cb14d29efe1cd7a99f00d05ee2d
Galka <gallkogluk@gmail.com>
2019-10-08 20:24:30 +0400

    feat: add answer to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let st=str.split('');
+  let arr=[];
+  for (let i=0; i<st.length; i++){               //перебираем строку-массив
+    
+    for (let e=0; e<bracketsConfig.length; e++){ //перебираем конфиг
+      
+      if (st[i]==bracketsConfig[e][0]){          //проверяем открыта ли скобка?
+        
+        if (bracketsConfig[e][0]==bracketsConfig[e][1]){ //проверяем на идентичность
+          if (arr.indexOf(bracketsConfig[e][0])!=-1){
+          arr.pop()}
+          else {arr.push(st[i])}
+        }
+        else {arr.push(st[i]);                          //закидываем в массив
+        }
+      }
+      else if(st[i]==bracketsConfig[e][1]){
+        if (arr.lastIndexOf(bracketsConfig[e][0])==(arr.length-1)){
+          arr.pop();
+        }
+        else{
+         return false;                                //удаляем из стекового массива
+        }
+      }
+    }
+  }
+  if (arr.length==0){
+    return true;
+  }
+  else {return false}
 }
